21f31a3fb977a563f8bf52cd08519c83ceeff4f2
Migue <miguelangelloppez7@gmail.com>
2024-05-17 16:13:01 +0200

    css

--- frontend/svc_frontend/.next/static/chunks/webpack.js
+++ frontend/svc_frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75d968d16972c9b6"; }
+/******/ 		__webpack_require__.h = function() { return "d7c1ba6da989aa84"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
