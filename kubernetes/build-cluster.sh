7145fb89f908dcec3294eab34c5d678357ac3ad2
Migue <miguelangelloppez7@gmail.com>
2024-05-18 03:55:05 +0200

    v1.0.0

--- kubernetes/build-cluster.sh
+++ kubernetes/build-cluster.sh
@@ -4,11 +4,13 @@ podman image rm -f backend
 podman image rm -f frontend
 cd ..
 cd backend
+rm -rf svc_backend/node_modules
 podman build -t backend .
 id_backend=$(podman inspect --format='{{.Id}}' backend)
 podman image push $id_backend docker.io/miguecrty/trabajorest:backend
 cd ..
 cd frontend
+rm -rf svc_frontend/node_modules
 podman build -t frontend .
 id_frontend=$(podman inspect --format='{{.Id}}' frontend)
 podman image push $id_frontend docker.io/miguecrty/trabajorest:frontend
